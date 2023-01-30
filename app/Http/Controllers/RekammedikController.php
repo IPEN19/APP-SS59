5c10af78e8fd7b3f3c90ee5f5ae7936ead09c2fd
IPEN19 <120349736+IPEN19@users.noreply.github.com>
2023-01-30 10:11:07 +0700

    Membuat Tampilan Rekam Medik

--- app/Http/Controllers/RekammedikController.php
+++ app/Http/Controllers/RekammedikController.php
@@ -13,7 +13,7 @@ class RekammedikController extends Controller
      */
     public function index()
     {
-        //
+        return view('rekammedik.index');
     }
 
     /**
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -251,7 +251,7 @@
         </div>
       </div><!-- /.container-fluid -->
     </section> --}}
-    @yield('conten-header')
+    @yield('content-header')
 
     <!-- Main content -->
     {{-- <section class="content">
@@ -282,17 +282,12 @@
       <!-- /.card -->
 
     </section> --}}
-    @yield('conten')
+    @yield('content')
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
